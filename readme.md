e36337e3a12ba39fc5599963cd9ba8cd8ee7fd2a
3ichael 7ambert <mlambertbusiness@gmail.com>
2023-07-30 17:02:44 -0600

    init

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Variables
+
+Similar to other languages, variables are defined as such
+```java
+int a;
+a = 12;
+double b;
+b = a+1
+```
+
+However the syntax may be shortened as such
+
+```java
+int a = 12;
+double b = a+1
+```
---
+++ variables.java
@@ -0,0 +1,11 @@
+package Java.variables-java;
+
+public class variables {
+    public static void main (String[] args)
+    {
+        int a = 12;
+        double b =  a + 2;
+        String s = "My First String"
+    }     // Add your code here to execute tasks or operations.
+}
+
